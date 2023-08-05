7e85dee4ec2255684e506b5036c060102e9adf8d
Jhonatan Felipe <83676609+jhonscarpa@users.noreply.github.com>
2023-08-05 11:50:39 -0300

    adicionando todos no reducer

--- src/components/AddTodo.tsx
+++ src/components/AddTodo.tsx
@@ -1,11 +1,15 @@
 import { FormEvent, useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { add } from '../store'
 
 export function AddTodo() {
+  const dispatch = useDispatch()
   const [newTodo, setNewTodo] = useState('')
 
   function handleNewTodo(e: FormEvent) {
     e.preventDefault()
     console.log(newTodo)
+    dispatch(add({ newTodo }))
   }
 
   return (
