63e70f4272ea876623a62a27350ceecf8e81e44c
Jhonatan Felipe <83676609+jhonscarpa@users.noreply.github.com>
2023-08-10 18:08:22 -0300

    add collapsible

--- src/components/Module.tsx
+++ src/components/Module.tsx
@@ -1,3 +1,4 @@
+import * as Collapsible from '@radix-ui/react-collapsible'
 import { ChevronDown } from 'lucide-react'
 import { Lesson } from './Lesson'
 
@@ -9,8 +10,8 @@ interface IPropsModule {
 
 export function Module({ title, amountOfLessons, moduleIndex }: IPropsModule) {
   return (
-    <div>
-      <button className="flex w-full items-center gap-3 bg-zinc-800 p-4">
+    <Collapsible.Root className="group ">
+      <Collapsible.Trigger className="flex w-full items-center gap-3 bg-zinc-800 p-4">
         <div className="flex h-10 w-10 rounded-full items-center justify-center bg-zinc-950 text-xs">
           {moduleIndex + 1}
         </div>
@@ -18,14 +19,16 @@ export function Module({ title, amountOfLessons, moduleIndex }: IPropsModule) {
           <strong className="text-sm">{title}</strong>
           <span className="text-xs text-zinc-400">{amountOfLessons} aulas</span>
         </div>
-        <ChevronDown className="w-5 h-5 ml-auto text-zinc-400" />
-      </button>
-      <nav className="relative flex flex-col gap-4 p-6">
-        <Lesson title="Conceitos do Redux" duration="10:33" />
-        <Lesson title="Conceitos do Redux" duration="10:33" />
-        <Lesson title="Conceitos do Redux" duration="10:33" />
-        <Lesson title="Conceitos do Redux" duration="10:33" />
-      </nav>
-    </div>
+        <ChevronDown className="w-5 h-5 ml-auto text-zinc-400 group-data-[state=open]:rotate-180 transition-transform" />
+      </Collapsible.Trigger>
+      <Collapsible.Content>
+        <nav className="relative flex flex-col gap-4 p-6">
+          <Lesson title="Conceitos do Redux" duration="10:33" />
+          <Lesson title="Conceitos do Redux" duration="10:33" />
+          <Lesson title="Conceitos do Redux" duration="10:33" />
+          <Lesson title="Conceitos do Redux" duration="10:33" />
+        </nav>
+      </Collapsible.Content>
+    </Collapsible.Root>
   )
 }
