9f11090cf35ef48f19fc8b786bd3b846f1e5a1b3
Jhonatan Felipe <83676609+jhonscarpa@users.noreply.github.com>
2023-08-12 20:54:13 -0300

    consume lessons

--- src/components/Module.tsx
+++ src/components/Module.tsx
@@ -1,6 +1,7 @@
 import * as Collapsible from '@radix-ui/react-collapsible'
 import { ChevronDown } from 'lucide-react'
 import { Lesson } from './Lesson'
+import { useAppSelector } from '../store'
 
 interface IPropsModule {
   moduleIndex: number
@@ -9,6 +10,9 @@ interface IPropsModule {
 }
 
 export function Module({ title, amountOfLessons, moduleIndex }: IPropsModule) {
+  const lessons = useAppSelector(
+    state => state.player.course.modules[moduleIndex].lessons,
+  )
   return (
     <Collapsible.Root className="group ">
       <Collapsible.Trigger className="flex w-full items-center gap-3 bg-zinc-800 p-4">
@@ -23,10 +27,13 @@ export function Module({ title, amountOfLessons, moduleIndex }: IPropsModule) {
       </Collapsible.Trigger>
       <Collapsible.Content>
         <nav className="relative flex flex-col gap-4 p-6">
-          <Lesson title="Conceitos do Redux" duration="10:33" />
-          <Lesson title="Conceitos do Redux" duration="10:33" />
-          <Lesson title="Conceitos do Redux" duration="10:33" />
-          <Lesson title="Conceitos do Redux" duration="10:33" />
+          {lessons.map(lesson => (
+            <Lesson
+              key={lesson.id}
+              title={lesson.title}
+              duration={lesson.duration}
+            />
+          ))}
         </nav>
       </Collapsible.Content>
     </Collapsible.Root>
