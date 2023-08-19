457c2a30639caef1f7720910d2be5797a56568c6
Jhonatan Felipe <83676609+jhonscarpa@users.noreply.github.com>
2023-08-19 19:59:12 -0300

    destaque info aula no header

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -2,7 +2,7 @@ import ReactPlayer from 'react-player'
 import { useAppSelector } from '../store'
 
 export function Video() {
-  const video = useAppSelector(state => {
+  const lesson = useAppSelector(state => {
     const { currentModuleIndex, currentLessonIndex } = state.player
 
     const currentLesson =
@@ -12,13 +12,14 @@ export function Video() {
 
     return currentLesson
   })
+
   return (
     <div className="w-full bg-zinc-950 aspect-video">
       <ReactPlayer
         width="100%"
         height="100%"
         controls
-        url={`https://www.youtube.com/watch?v=${video.id}`}
+        url={`https://www.youtube.com/watch?v=${lesson.id}`}
       />
     </div>
   )
