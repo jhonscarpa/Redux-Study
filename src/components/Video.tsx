39feba3a356879266eed38de7d35ca8a40c7a302
Jhonatan Felipe <83676609+jhonscarpa@users.noreply.github.com>
2023-08-20 22:37:11 -0300

    autoplay next class

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,7 +1,10 @@
 import ReactPlayer from 'react-player'
 import { useAppSelector } from '../store'
+import { useDispatch } from 'react-redux'
+import { next } from '../store/slices/player'
 
 export function Video() {
+  const dispatch = useDispatch()
   const lesson = useAppSelector(state => {
     const { currentModuleIndex, currentLessonIndex } = state.player
 
@@ -13,6 +16,10 @@ export function Video() {
     return currentLesson
   })
 
+  function handlePlayNext() {
+    dispatch(next())
+  }
+
   return (
     <div className="w-full bg-zinc-950 aspect-video">
       <ReactPlayer
@@ -20,6 +27,8 @@ export function Video() {
         height="100%"
         controls
         url={`https://www.youtube.com/watch?v=${lesson.id}`}
+        playing
+        onEnded={handlePlayNext}
       />
     </div>
   )
