d82e083e41ee9b9fcd57e4f75360be9ad9d566ea
Jhonatan Felipe <83676609+jhonscarpa@users.noreply.github.com>
2023-08-21 17:50:56 -0300

    create useHook useCurrentLesson

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,20 +1,10 @@
 import ReactPlayer from 'react-player'
-import { useAppSelector } from '../store'
 import { useDispatch } from 'react-redux'
-import { next } from '../store/slices/player'
+import { next, useCurrentLesson } from '../store/slices/player'
 
 export function Video() {
   const dispatch = useDispatch()
-  const lesson = useAppSelector(state => {
-    const { currentModuleIndex, currentLessonIndex } = state.player
-
-    const currentLesson =
-      state.player.course.modules[currentModuleIndex].lessons[
-        currentLessonIndex
-      ]
-
-    return currentLesson
-  })
+  const { currentLesson } = useCurrentLesson()
 
   function handlePlayNext() {
     dispatch(next())
@@ -26,7 +16,7 @@ export function Video() {
         width="100%"
         height="100%"
         controls
-        url={`https://www.youtube.com/watch?v=${lesson.id}`}
+        url={`https://www.youtube.com/watch?v=${currentLesson.id}`}
         playing
         onEnded={handlePlayNext}
       />
