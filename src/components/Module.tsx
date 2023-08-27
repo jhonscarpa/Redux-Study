ba908bba5a326f70c6acc6478f7f987d4176341f
Jhonatan Felipe <83676609+jhonscarpa@users.noreply.github.com>
2023-08-26 23:08:02 -0300

    getting start info api reducer

--- src/components/Module.tsx
+++ src/components/Module.tsx
@@ -1,8 +1,7 @@
 import * as Collapsible from '@radix-ui/react-collapsible'
 import { ChevronDown } from 'lucide-react'
 import { Lesson } from './Lesson'
-import { useAppSelector } from '../store'
-import { useDispatch } from 'react-redux'
+import { useAppDispatch, useAppSelector } from '../store'
 import { play } from '../store/slices/player'
 
 interface IPropsModule {
@@ -12,7 +11,7 @@ interface IPropsModule {
 }
 
 export function Module({ title, amountOfLessons, moduleIndex }: IPropsModule) {
-  const dispatch = useDispatch()
+  const dispatch = useAppDispatch()
 
   const lessons = useAppSelector(
     state => state.player.course?.modules[moduleIndex].lessons,
