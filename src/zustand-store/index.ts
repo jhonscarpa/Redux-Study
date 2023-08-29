d4738e5c17ce20a4d39193f15cc4789abdc6423f
Jhonatan Felipe <83676609+jhonscarpa@users.noreply.github.com>
2023-08-29 18:40:41 -0300

    get data with zustand and create loading

--- src/zustand-store/index.ts
+++ src/zustand-store/index.ts
@@ -1,4 +1,5 @@
 import { create } from 'zustand'
+import { api } from '../lib/axios'
 
 interface IPropsCourse {
   id: number
@@ -20,6 +21,7 @@ export interface IPropsPlayerState {
   isLoading: boolean
   play: (moduleAndLessonIndex: [number, number]) => void
   next: () => void
+  load: () => Promise<void>
 }
 
 export const useStore = create<IPropsPlayerState>((set, get) => {
@@ -28,6 +30,17 @@ export const useStore = create<IPropsPlayerState>((set, get) => {
     currentModuleIndex: 0,
     currentLessonIndex: 0,
     isLoading: true,
+
+    load: async () => {
+      set({
+        isLoading: true,
+      })
+      const response = await api.get('/courses/1')
+      set({
+        course: response.data,
+        isLoading: false,
+      })
+    },
 
     play: (moduleAndLessonIndex: [number, number]) => {
       const [moduleIndex, lessonIndex] = moduleAndLessonIndex
